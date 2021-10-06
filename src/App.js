5d08a23918ccbf6be974e588b0052567904dd311
cashrewards\ignacio.barreto <ignacio.barreto@cashrewards.com>
2021-10-06 11:45:57 +1100

    tercera clase menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Title} from './Title';
+import {NavBar} from './components/NavBar';
 
 function App() {
+
+  const alertName = () => alert('Yo');
+
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+         <NavBar />
+      <header className="App-header">             
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <Title />         
         </p>
+      
+        <button onClick={alertName}> click me 
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Ignacio
         </a>
       </header>
     </div>
