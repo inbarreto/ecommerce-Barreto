17e591bff05de8e3d8208f2f3dfd5cc3d9b341f4
cashrewards\ignacio.barreto <ignacio.barreto@cashrewards.com>
2021-11-08 12:11:36 +1100

    replace to json, and change variables

--- src/App.js
+++ src/App.js
@@ -5,27 +5,39 @@ import { NavBar } from "./components/NavBar/NavBar";
 //import { Title } from "./components/Title/Title";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
 function App() {
   // const alertName = () => alert("Yo");
 
   // const [title, setTitle] = useState("My E-commerce 2");
 
   // const handleTitle = () => setTitle("My e-Comm");
+  const switchDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    console.log(isDarkMode);
+  };
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <div className="App">
-            <ItemListContainer greeting="" />
-          </div>
-        </Route>
-        <Route exact path="/Detail">
-          <ItemDetailContainer />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <ThemeContext.Provider value={isDarkMode}>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
+              <ItemListContainer greeting="" />
+              <button onClick={switchDarkMode}>TESTING</button>
+            </div>
+          </Route>
+          <Route exact path="/Detail">
+            <ItemDetailContainer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,72 +1,75 @@
 import "./ItemDetailContainer.css";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import { useState, useEffect } from "react";
+import Pants from "./../../Data/pants.json";
 
 export const ItemDetailContainer = (props) => {
-  const [listPants, updatePants] = useState(null);
-  const [listShoes, updateShoes] = useState(null);
+  const [listPants, setPants] = useState(null);
+  const [listShoes, setShoes] = useState(null);
 
-  useEffect(() => {
-    getItem();
-  }, [updatePants]);
-
-  const getItem = () => {
-    const task = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
+  const getItem = (data) =>
+    new Promise((resolve, reject) => {
+      if (data) {
+        resolve(data);
+      } else reject("No hay data.");
     });
 
-    task
-      .then(
-        (result) => {
-          updatePants([
-            {
-              Id: 1,
-              Price: 120,
-              Item: "Wranglers",
-              Description: "Lose fit",
-              Imagen:
-                "https://media.everlane.com/image/upload/c_fill,dpr_2.0,f_auto,g_face,q_auto,w_auto:100:702/v1/i/613846fc_009d.jpg",
-              Stock: 1,
-            },
-            {
-              Id: 2,
-              Price: 340,
-              Item: "Lacoste",
-              Description: "Fit",
-              Imagen:
-                "https://images.asos-media.com/products/nike-city-made-pack-woven-cargo-pants-in-black/21456879-1-black?$n_640w$&wid=513&fit=constrain",
-              Stock: 1,
-            },
-          ]);
+  useEffect(() => {
+    getItem(Pants)
+      .then((res) => setPants(res))
+      .catch((err) => console.log(err));
+  });
 
-          updateShoes([
-            {
-              Id: 3,
-              Price: 110,
-              Item: "Nike",
-              Description: "Lose",
-              Imagen:
-                "https://assets.kogan.com/files/IT_hardware_products/NIK-BQ3204-002-8-1.jpg?auto=webp&canvas=753%2C502&fit=bounds&height=502&quality=75&width=753",
-              Stock: 1,
-            },
-            {
-              Id: 4,
-              Price: 120,
-              Item: "Adidas",
-              Description: "Fit",
-              Imagen:
-                "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/168bdd9ab43041158653a800001d546a_9366/Seeley_Shoes_Black_F37427_01_standard.jpg",
-              Stock: 1,
-            },
-          ]);
-        },
-        (err) => {}
-      )
-      .catch((err) => {})
-      .finally((err) => {});
-  };
+  //   task
+  //     .then(
+  //       (result) => {
+  //         updatePants([
+  //           {
+  //             Id: 1,
+  //             Price: 120,
+  //             Item: "Wranglers",
+  //             Description: "Lose fit",
+  //             Imagen:
+  //               "https://media.everlane.com/image/upload/c_fill,dpr_2.0,f_auto,g_face,q_auto,w_auto:100:702/v1/i/613846fc_009d.jpg",
+  //             Stock: 1,
+  //           },
+  //           {
+  //             Id: 2,
+  //             Price: 340,
+  //             Item: "Lacoste",
+  //             Description: "Fit",
+  //             Imagen:
+  //               "https://images.asos-media.com/products/nike-city-made-pack-woven-cargo-pants-in-black/21456879-1-black?$n_640w$&wid=513&fit=constrain",
+  //             Stock: 1,
+  //           },
+  //         ]);
+
+  //         updateShoes([
+  //           {
+  //             Id: 3,
+  //             Price: 110,
+  //             Item: "Nike",
+  //             Description: "Lose",
+  //             Imagen:
+  //               "https://assets.kogan.com/files/IT_hardware_products/NIK-BQ3204-002-8-1.jpg?auto=webp&canvas=753%2C502&fit=bounds&height=502&quality=75&width=753",
+  //             Stock: 1,
+  //           },
+  //           {
+  //             Id: 4,
+  //             Price: 120,
+  //             Item: "Adidas",
+  //             Description: "Fit",
+  //             Imagen:
+  //               "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/168bdd9ab43041158653a800001d546a_9366/Seeley_Shoes_Black_F37427_01_standard.jpg",
+  //             Stock: 1,
+  //           },
+  //         ]);
+  //       },
+  //       (err) => {}
+  //     )
+  //     .catch((err) => {})
+  //     .finally((err) => {});
+  // };
 
   return (
     <>
